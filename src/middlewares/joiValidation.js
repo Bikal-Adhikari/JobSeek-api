d5663574bce2172f24cc0e66244baa67e436ea30
Bikal <bikaladhikari5@gmail.com>
2024-06-02 19:22:15 +1000

    new user validation is created using joi

---
+++ src/middlewares/joiValidation.js
@@ -0,0 +1,34 @@
+import Joi from "joi";
+
+const STR_REQUIRED = Joi.string().required();
+// const STR = Joi.string()
+const PHONE = Joi.string().allow("", null);
+const EMAIL = Joi.string().email({
+  minDomainSegments: 2,
+  tlds: { allow: ["com", "net"] },
+});
+
+const joiValidator = ({ req, res, next, schema }) => {
+  try {
+    const { error } = schema.validate(req.body);
+    error
+      ? res.json({
+          status: "error",
+          message: error.message,
+        })
+      : next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const newUserValidation = (req, res, next) => {
+  const schema = Joi.object({
+    fName: STR_REQUIRED,
+    lName: STR_REQUIRED,
+    phone: PHONE,
+    email: EMAIL,
+    password: STR_REQUIRED,
+  });
+  return joiValidator({ req, res, next, schema });
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,39 @@
 import express from "express";
+import { newUserValidation } from "../middlewares/joiValidation.js";
 
 const router = express.Router();
+
+router.all("/", (req, res, next) => {
+  //always execute
+  console.log("from all");
+  next();
+});
+
+//======public controllers======
+
+//create new user
+router.post("/", newUserValidation, async (req, res, next) => {
+  try {
+    req.body.password = hashPassword(req.body.password);
+    const user = await createNewUser(req.body);
+
+    user?._id
+      ? res.json({
+          status: "success",
+          message: "Your Account has been created successfully",
+        })
+      : res.json({
+          status: "failure",
+          message: "Unable to create an account, try again later",
+        });
+  } catch (error) {
+    if (error.message.includes("E11000 duplicate key")) {
+      error.message =
+        "Another user already have this email, change your email and try again";
+      error.status = 200;
+    }
+    next(error);
+  }
+});
 
 export default router;
